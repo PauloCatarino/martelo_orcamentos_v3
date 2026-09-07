bf372af8661d9e4431442e80850e23816184c861
PauloCatarino <flash_azul@hotmail.com>
2026-09-07 16:50:46 +0100

    Fora os ficheiros do guia narrado que ninguem usa

--- app/ui/ajuda/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Conteúdo local e versionado do Centro de Ajuda."""
-
-from app.ui.ajuda.guias import GuiaAjuda, PassoGuia, carregar_guias
-
-__all__ = ["GuiaAjuda", "PassoGuia", "carregar_guias"]
